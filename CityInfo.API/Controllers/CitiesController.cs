e3e7dea255dff853e2d959d208610386b97168ba
sundarDeveloper <sundar.sambandam@gmail.com>
2018-12-27 14:21:12 +0000

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+  [Route("api/[controller]")]
+  public class CitiesController : Controller
+  {
+    // GET api/values
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+      var cityToReturn= CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id ==  id);
+      if (cityToReturn == null)
+      {
+        return NotFound();
+      }
+      return Ok(cityToReturn);
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+  [Route("api/Cities")]
+  public class PointsOfInterestController : Controller
+  {
+    [HttpGet("{cityId}/pointsofinterest")]
+    public IActionResult GetPointsOfInterest(int cityId)
+    {
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+      if (city == null)
+      {
+        return NotFound();
+      }
+      var poi = city.PointsOfInterest;
+      if (poi == null)
+      {
+        return NotFound();
+      }
+      return Ok(poi);
+    }
+
+    [HttpGet("{cityId}/pointsofinterest/{Id}")]
+    public IActionResult GetPointsOfInterest(int cityId, int Id)
+    {
+      var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+      if (city == null)
+      {
+        return NotFound();
+      }
+      var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == Id);
+      if (poi == null)
+      {
+        return NotFound();
+      }
+      return Ok(poi);
+    }
+  }
+}
